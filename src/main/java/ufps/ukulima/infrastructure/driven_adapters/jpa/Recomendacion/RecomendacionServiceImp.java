159244715bd592bb765850377961a4645a02d45d
Santiago Serrano <62350078+SantiagoAndresSerrano@users.noreply.github.com>
2022-08-01 11:25:57 -0500

    Update RecomendacionServiceImp.java

--- src/main/java/ufps/ukulima/infrastructure/driven_adapters/jpa/Recomendacion/RecomendacionServiceImp.java
+++ src/main/java/ufps/ukulima/infrastructure/driven_adapters/jpa/Recomendacion/RecomendacionServiceImp.java
@@ -1,6 +1,23 @@
 package ufps.ukulima.infrastructure.driven_adapters.jpa.Recomendacion;
 
+import ufps.ukulima.domain.model.Recomendacion.Recomendacion;
 import ufps.ukulima.domain.model.Recomendacion.gateway.RecomendacionService;
 
+import java.util.List;
+
 public class RecomendacionServiceImp implements RecomendacionService {
+    @Override
+    public void saveRecomendacion(Recomendacion recomendacion) {
+        
+    }
+
+    @Override
+    public Recomendacion findRecomendacionById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Recomendacion> findByAllRecomendacion() {
+        return null;
+    }
 }
